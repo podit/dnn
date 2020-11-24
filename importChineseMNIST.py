89805ebae618cd2ed112bc6585deff3d6cdb0bfd
podit <tomody@outlook.com>
2020-11-24 03:02:42 +0000

    arranged datasets and fixed data filepaths

--- importChineseMNIST.py
+++ importChineseMNIST.py
@@ -5,8 +5,8 @@ class imp:
     # Import class for use with jpg input data and csv label set
     #   Chinese MNIST - https://www.kaggle.com/gpreda/chinese-mnist
     def __init__(self):
-        self.mndata = 'data'
-        self.mninput = self.mndata + '/data/data'
+        self.mndata = 'data/chinese'
+        self.mninput = self.mndata + '/data'
         self.mnlabel = self.mndata + '/chinese_mnist.csv'
         self.classes = 15
         self.samples = 1000
